2a03d190891e095c6d28f7cdc0b2fec57a57d206
beepboop <89303943+Discotijger@users.noreply.github.com>
2026-04-01 13:22:52 +0200

    Movement Functional

--- JS/Index.js
+++ JS/Index.js
@@ -1,9 +1,52 @@
 let Player = document.getElementById("Character");
 
+let x = 0;
+let movingR = false;
+let movingL = false;
 
 
-let input = document.addEventListener("keydown", function (event) {
-            console.log("You pressed a key:", event.key);
-            });
+document.addEventListener("keydown", (event) => {
+  if (event.key === "d" || event.key === "D") {
+    movingR = true;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "d" || event.key === "D") {
+    movingR = false;
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "a" || event.key === "A") {
+    movingL = true;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "a" || event.key === "A") {
+    movingL = false;
+  }
+});
 
 
+
+
+
+
+function Movement() {
+    if (movingR){
+        x += 2; // speed
+        Player.style.left = x + "px";
+    }
+  
+    if (movingL){
+        x -= 2;
+        Player.style.left = x + "px";
+    }
+    requestAnimationFrame(Movement);
+}
+
+
+
+Movement();
